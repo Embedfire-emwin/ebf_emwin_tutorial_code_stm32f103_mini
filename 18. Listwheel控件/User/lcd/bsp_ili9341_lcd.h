f5c9b7621bce9276f979948b1c3150092beb53d6
Amber <1442312382@qq.com>
2021-08-09 11:54:55 +0800

    修改读LCD驱动芯片id函数，增加对新液晶屏驱动ic ST7789的支持 修改原本不正确的 ILI9341_Read_PixelData 函数

--- 18. Listwheel控件/User/lcd/bsp_ili9341_lcd.h
+++ 18. Listwheel控件/User/lcd/bsp_ili9341_lcd.h
@@ -127,6 +127,13 @@ extern uint8_t LCD_SCAN_MODE;
 #define      CMD_SetPixel		 		          0x2C	     //填充像素
 
 
+/* 定义 LCD 驱动芯片 ID */
+#define     LCDID_UNKNOWN             0
+#define     LCDID_ILI9341             0x9341
+#define     LCDID_ST7789V             0x8552
+
+
+extern uint16_t lcdid;
 
 
 /********************************** 声明 ILI934 函数 ***************************************/
@@ -153,7 +160,7 @@ void 											LCD_SetBackColor								(uint16_t Color);
 void 											LCD_SetTextColor								(uint16_t Color)	;
 void 											LCD_SetColors										(uint16_t TextColor, uint16_t BackColor);
 void 											LCD_GetColors										(uint16_t *TextColor, uint16_t *BackColor);
-uint16_t 								ILI9341_Read_ID									(void);
+uint16_t 								ILI9341_ReadID									(void);
 
 
 __inline void                 ILI9341_Write_Cmd           ( uint16_t usCmd );
